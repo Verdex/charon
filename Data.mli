94eef4ad70efecb933d0dbcba62141ef301025c1
David L. Inman <davidlevere@gmail.com>
2019-11-23 16:55:47 -0500

    Data and lexer

---
+++ Data.mli
@@ -0,0 +1,18 @@
+
+type token = 
+    | String of string
+    | Number of string
+    | Symbol of string
+    | RParen
+    | LParen
+    | RCurl
+    | LCurl
+    | Orbar
+    | Colon
+    | SemiColon
+    | RAngle
+    | LAngle
+    | Comma
+    | RightArrow 
+    | Equal 
+
